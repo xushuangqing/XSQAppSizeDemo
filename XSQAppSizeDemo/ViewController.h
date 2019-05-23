8ea3e6c6170c0195e180524cda6ebb871123984e
xushuangqing <xushuangqing@bytedance.com>
2019-05-23 17:29:17 +0800

    Initial Commit

---
+++ XSQAppSizeDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XSQAppSizeDemo
+//
+//  Created by xushuangqing on 2019/5/23.
+//  Copyright © 2019 xushuangqing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
