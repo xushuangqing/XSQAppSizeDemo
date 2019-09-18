df256f079c67e3b72a4e5fe09ce69c5b5fea3522
xushuangqing <xushuangqing@bytedance.com>
2019-09-18 16:17:24 +0800

    测试静态库

---
+++ XSQAppSizeDemo/XSQHello.h
@@ -0,0 +1,17 @@
+//
+//  XSQHello.h
+//  XSQAppSizeDemo
+//
+//  Created by xushuangqing on 2019/9/18.
+//  Copyright © 2019 xushuangqing. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface XSQHello : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ XSQAppSizeDemo/XSQHello.m
@@ -0,0 +1,13 @@
+//
+//  XSQHello.m
+//  XSQAppSizeDemo
+//
+//  Created by xushuangqing on 2019/9/18.
+//  Copyright © 2019 xushuangqing. All rights reserved.
+//
+
+#import "XSQHello.h"
+
+@implementation XSQHello
+
+@end
